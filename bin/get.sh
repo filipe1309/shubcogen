6ef93b7dbebe00c20fa0a960a0f11dfb5fbc095a
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-05-06 21:30:31 -0300

    chore: change get script to downalod deploy into bin folder

--- bin/get.sh
+++ bin/get.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/filipe1309/skeleton-courses/main/bin/deploy.sh
+wget https://raw.githubusercontent.com/filipe1309/skeleton-courses/main/bin/deploy.sh -P bin
 wget https://raw.githubusercontent.com/filipe1309/skeleton-courses/main/template.md -O README.md
 wget https://raw.githubusercontent.com/filipe1309/skeleton-courses/main/notes.md
