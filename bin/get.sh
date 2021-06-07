a2c82807aff5306ad81e5271bab66489136bb65e
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-06-07 01:04:37 -0300

    chore: add license file

--- bin/get.sh
+++ bin/get.sh
@@ -3,5 +3,6 @@
 wget https://raw.githubusercontent.com/filipe1309/skeleton-courses/main/bin/deploy.sh -P bin
 wget https://raw.githubusercontent.com/filipe1309/skeleton-courses/main/templates/README.md -O README.md
 wget https://raw.githubusercontent.com/filipe1309/skeleton-courses/main/templates/notes.md
+wget https://raw.githubusercontent.com/filipe1309/skeleton-courses/main/LICENSE
 
 chmod +x bin/deploy.sh
