c131efe9b431d6ee73db027c8471e2c4bd600273
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-08-12 15:28:05 -0300

    feat: improve colors & finish msg

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,9 +2,25 @@
 
 # DevOntheRun Deploy Script
 
+# Black        0;30     Dark Gray     1;30
+# Red          0;31     Light Red     1;31
+# Green        0;32     Light Green   1;32
+# Brown/Orange 0;33     Yellow        1;33
+# Blue         0;34     Light Blue    1;34
+# Purple       0;35     Light Purple  1;35
+# Cyan         0;36     Light Cyan    1;36
+# Light Gray   0;37     White         1;37
+
+BG_GREEN='\e[1;32m'
+NO_BG='\e[m'
+DARK_GRAY='\033[1;30m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+echo -e "${BG_GREEN}"
 echo "#############################################"
 echo "                   DEPLOY                   "
-echo "#############################################"
+echo -e "#############################################${NO_BG}"
 echo "# [Optional] param: --tag-msg \"TAG_MESSAGE_HERE\""
 echo "---------------------------------------------"
 
@@ -85,7 +101,7 @@ if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
     git add notes.md && git commit -m "docs: update notes"
     confirm "Checkout to \"$GIT_DEFAULT_BRANCH\" branch & Merge current branch ($GIT_BRANCH)? [Y/n]" && git checkout $GIT_DEFAULT_BRANCH && git pull && git merge $GIT_BRANCH
     confirm "Deploy on \"$GIT_DEFAULT_BRANCH\" branch? [Y/n]" git push origin $GIT_DEFAULT_BRANCH && git push origin $GIT_DEFAULT_BRANCH --tags
-    echo "Deploy completed!"
+    echo -e "                   \xE2\x9C\x94 DEPLOY COMPLETED"
     confirm "Go to next class/episode? ($GIT_BRANCH_NEXT_CLASS_LW) [Y/n]" && git checkout -b $GIT_BRANCH_NEXT_CLASS_LW
     echo "## ${GIT_BRANCH_NEXT_CLASS^^}" >> notes.md
     echo "" >> notes.md
