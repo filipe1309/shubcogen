746b5441fc4906070a0f921b580f6f78316b0602
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-08-12 22:43:45 -0300

    Update version (revision) from 0.8.5 to 0.8.6

--- .shub/bin/get.sh
+++ .shub/bin/get.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -o .shub/bin/deploy.sh https://raw.githubusercontent.com/filipe1309/shubcogen/main/.shub/bin/deploy.sh
-curl -o .shub/bin/update.sh https://raw.githubusercontent.com/filipe1309/shubcogen/main/.shub/bin/update.sh
-curl -o .shub/bin/version https://raw.githubusercontent.com/filipe1309/shubcogen/main/.shub/bin/version
+curl -o .shub/bin/deploy.sh --create-dirs https://raw.githubusercontent.com/filipe1309/shubcogen/main/.shub/bin/deploy.sh
+curl -o .shub/bin/update.sh --create-dirs https://raw.githubusercontent.com/filipe1309/shubcogen/main/.shub/bin/update.sh
+curl -o .shub/bin/version --create-dirs https://raw.githubusercontent.com/filipe1309/shubcogen/main/.shub/bin/version
 
 curl -o README.md https://raw.githubusercontent.com/filipe1309/shubcogen/main/templates/README.md
 curl -o notes.md https://raw.githubusercontent.com/filipe1309/shubcogen/main/templates/notes.md
