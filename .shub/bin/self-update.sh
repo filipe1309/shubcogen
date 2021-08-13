b50549ef2c077e6639623ced988f98149d6052b6
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-08-13 01:01:33 -0300

    fix: add self-update on its self

--- .shub/bin/self-update.sh
+++ .shub/bin/self-update.sh
@@ -12,6 +12,7 @@ if [ "$REMOTE_VERSION" != "$LOCAL_VERSION" ]; then
             echo "Updating..."
             curl -o .shub/bin/version https://raw.githubusercontent.com/filipe1309/shubcogen/main/.shub/bin/version
             curl -o .shub/bin/deploy.sh https://raw.githubusercontent.com/filipe1309/shubcogen/main/.shub/bin/deploy.sh
+            curl -o .shub/bin/self-update.sh https://raw.githubusercontent.com/filipe1309/shubcogen/main/.shub/bin/self-update.sh
             chmod +x .shub/bin/deploy.sh
             exit 0
         fi
