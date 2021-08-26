f6be642a10b9435e850822e8e8f7b71dcc5d2917
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-08-26 20:34:39 -0300

    chore: update shub files

---
+++ .shub/bin/colors.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# DevOntheRun Colors Script
+
+.shub/bin/shub-logo.sh
+
+
+# Black        0;30     Dark Gray     1;30
+# Red          0;31     Light Red     1;31
+# Green        0;32     Light Green   1;32
+# Brown/Orange 0;33     Yellow        1;33
+# Blue         0;34     Light Blue    1;34
+# Purple       0;35     Light Purple  1;35
+# Cyan         0;36     Light Cyan    1;36
+# Light Gray   0;37     White         1;37
+
+BG_GREEN='\e[1;32m'
+NO_BG='\e[m'
+DARK_GRAY='\033[1;30m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
--- .shub/bin/deploy.sh
+++ .shub/bin/deploy.sh
@@ -4,21 +4,7 @@
 
 .shub/bin/shub-logo.sh
 
-
-# Black        0;30     Dark Gray     1;30
-# Red          0;31     Light Red     1;31
-# Green        0;32     Light Green   1;32
-# Brown/Orange 0;33     Yellow        1;33
-# Blue         0;34     Light Blue    1;34
-# Purple       0;35     Light Purple  1;35
-# Cyan         0;36     Light Cyan    1;36
-# Light Gray   0;37     White         1;37
-
-BG_GREEN='\e[1;32m'
-NO_BG='\e[m'
-DARK_GRAY='\033[1;30m'
-RED='\033[0;31m'
-NC='\033[0m' # No Color
+.shub/bin/colors.sh
 
 VERSION=$(head -n 1 .shub/bin/version)
 
