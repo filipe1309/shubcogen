5ea0c763d6d246ddb1d383407646270b73068fe9
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-08-27 23:17:42 -0300

    feat: add helpers file

---
+++ .shub/bin/helpers.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# DevOntheRun Helpers Script
+
+function parse_json() {
+    echo $1 | \
+    sed -e 's/[{}]/''/g' | \
+    sed -e 's/", "/'\",\"'/g' | \
+    sed -e 's/" ,"/'\",\"'/g' | \
+    sed -e 's/" , "/'\",\"'/g' | \
+    sed -e 's/","/'\"---SEPERATOR---\"'/g' | \
+    awk -F=':' -v RS='---SEPERATOR---' "\$1~/\"$2\"/ {print}" | \
+    sed -e "s/\"$2\"://" | \
+    tr -d "\n\t" | \
+    sed -e 's/\\"/"/g' | \
+    sed -e 's/\\\\/\\/g' | \
+    sed -e 's/^[ \t]*//g' | \
+    sed -e 's/^"//'  -e 's/"$//' | \
+    sed -e 's/"//' | \
+    sed -e 's/ $//'
+}
+
+function confirm() {
+    read -r -p "${1:-Are you sure? [Y/n]} " response
+    response=${response,,} # tolower
+    if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
+        echo "Ok"
+    else
+        exit 0;
+    fi
+}
