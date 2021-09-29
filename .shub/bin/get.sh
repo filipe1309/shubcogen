a9393516290ec5b61115aabd72c34900efc8f9bf
Filipe Leuch Bonfim <filipe1309@gmail.com>
2021-09-29 00:40:28 -0300

    tests: add tests to get sh templates

--- .shub/bin/get.sh
+++ .shub/bin/get.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
+TEMPLATE_DIR=""
+
+[ $ENV == "test" ] && echo "TEST MODE"
+[ $ENV != "test" ] && echo "NORMAL MODE"
+
+
+if [ $ENV == "test" ]; then
+    TEMPLATE_DIR="test/out/"
+    # $1
+fi
+
+
 clear
 
 echo "---------------------------------------------"
 
-backup() {
-    echo "---------------------------------------------"
-    file="$1"
-    if [ -f "$file" ]; then
-        echo "Backing up $file"
-        mkdir -p .shub/backup && cp "$file" ".shub/backup/$file"
-    fi 
+backupTemplateFiles() {
+    backup() {
+        echo "---------------------------------------------"
+        file="$1"
+        if [ -f "$file" ]; then
+            echo "Backing up $file"
+            mkdir -p .shub/backup && cp "$file" ".shub/backup/$file"
+        fi 
+    }
+    echo ""
+    backup "README.md"
+    backup "notes.md"
 }
-
-echo ""
-
-echo "Backing up files into .shub/backup ..."
-backup "README.md"
-backup "notes.md"
+[ $ENV != "test" ] && backupTemplateFiles
 
 echo ""
 echo "---------------------------------------------"
@@ -27,14 +39,22 @@ echo ""
 echo "---------------------------------------------"
 
 # Script files
-curl -o .shub/bin/links.txt --create-dirs https://raw.githubusercontent.com/filipe1309/shubcogen/main/.shub/bin/links.txt
-cat .shub/bin/links.txt | while read CMD; do curl -o $(echo ".shub/bin/$(basename $CMD) --create-dirs $CMD"); done;
-chmod -R +x .shub/bin/*.sh
-ln -s .shub/bin/deploy.sh shub-deploy.sh
+downloadScripts() {
+    curl -o .shub/bin/links.txt --create-dirs https://raw.githubusercontent.com/filipe1309/shubcogen/main/.shub/bin/links.txt
+    cat .shub/bin/links.txt | while read CMD; do curl -o $(echo ".shub/bin/$(basename $CMD) --create-dirs $CMD"); done;
+    chmod -R +x .shub/bin/*.sh
+    ln -s .shub/bin/deploy.sh shub-deploy.sh
+}
+[ $ENV != "test" ] && downloadScripts
 
 # Template files
-curl -o README.md https://raw.githubusercontent.com/filipe1309/shubcogen/main/templates/README.md
-curl -o notes.md https://raw.githubusercontent.com/filipe1309/shubcogen/main/templates/notes.md
-curl -o LICENSE https://raw.githubusercontent.com/filipe1309/shubcogen/main/LICENSE
+downloadTemplates() {
+    curl -o "$TEMPLATE_DIR""README.md" https://raw.githubusercontent.com/filipe1309/shubcogen/main/templates/README.md
+    curl -o "$TEMPLATE_DIR""notes.md" https://raw.githubusercontent.com/filipe1309/shubcogen/main/templates/notes.md
+    curl -o "$TEMPLATE_DIR""LICENSE" https://raw.githubusercontent.com/filipe1309/shubcogen/main/LICENSE
+}
+[ $ENV != "test" ] && downloadTemplates
 
-.shub/bin/init.sh
+[ $ENV != "test" ] && .shub/bin/init.sh
+
+[ $ENV == "test" ] && $1
